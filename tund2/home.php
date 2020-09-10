dcf85d22939d784955360f3d9a18832300bbaecb
Tanel Volkov <tanelvo@users.noreply.github.com>
2020-09-10 17:39:40 +0300

    Kodunetöö

--- tund2/home.php
+++ tund2/home.php
@@ -6,11 +6,17 @@ $partofday = "lihtsalt aeg" ;
 if($hournow < 6){
 	$partofday = "uneaeg";
 }
-if($hournow >= 6 and $hournow < 8) {
+if($hournow >= 7 and $hournow < 8) {
 	$partofday = "Hommikuste protseduuride aeg";
 }
-if($hournow >= 8 and $hournow < 18) {
+if($hournow >= 8 and $hournow < 16) {
 	$partofday = "Õppimise aeg";
+}
+if($hournow >= 16 and $hournow < 21) {
+	$partofday = "Töövahetus";
+}
+if($hournow >= 21 and $hournow < 0) {
+	$partofday = "Vaba aeg";
 }
 
 //jälgime semestri kulgu
@@ -20,6 +26,11 @@ $semesterduration = $semesterstart->diff($semesterend);
 $today = new DateTime("now");
 $fromsemesterstart = $semesterstart->diff($today); //Saime aja erinevuse objektiivina, seda niisama näidata ei saa
 $fromsemesterstartdays = $fromsemesterstart->format("%r%a");
+$fromsemesterend = $semesterend->diff($today);
+$fromsemesterenddays = $fromsemesterend->format("%r%a");
+
+$semesterprocent = 100*$fromsemesterstartdays/103
+
 ?>
 <!DOCTYPE html>
 <html lang="et">
@@ -34,7 +45,16 @@ $fromsemesterstartdays = $fromsemesterstart->format("%r%a");
  <h1><?php echo $username; ?> programmeerib veebi</h1>
   <p>See veebileht on loodud õppetöö kaigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
   <p>Leht on loodud veebiprogrammeerimise kursusel <a href="http://www.tlu.ee">Tallinna ülikooli</a> Digitehnoloogiate instituudis.</p>
-<p> Lehe avamise aeg: <?php echo $fulltimenow .", semestri algusest on möödunud " .$fromsemesterstartdays ." päeva"; ?>. 
-<?php echo "Parajasti on " .$partofday. "."; ?> </p>
+  
+<p> Lehe avamise aeg: <?php echo $fulltimenow .", semestri algusest on möödunud " .$fromsemesterstartdays ." päeva"; ?>. </p>
+
+<p> Semestri lõpuni on <?php echo $fromsemesterenddays*-1 ?> päeva.</p>
+
+<p> Semester on <?php if($fromsemesterenddays < 0){
+	echo($semesteropen = "käimas. ");}
+	else {echo("läbi. ");}	
+	echo number_format($semesterprocent, 1);?>% semestrist on läbitud. </p>
+	
+<p><?php echo "Parajasti on " .$partofday. "."; ?> </p>
 </body>
 </html>
